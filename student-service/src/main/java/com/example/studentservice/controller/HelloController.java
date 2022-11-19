4bf5763f1b1c5b6c41e069c14806e420243e970b
pouchine <1291980381@qq.com>
2022-11-19 14:50:34 +0800

    first commit

---
+++ student-service/src/main/java/com/example/studentservice/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.studentservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author pouchine
+ * @email 120L022131@stu.hit.edu.cn
+ * @create 2022/11/19 13:13
+ */
+@Controller
+@RequestMapping("/Hello")
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/world")
+    public String hello(){
+        return "hello";
+    }
+}
