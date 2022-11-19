2a173634c9296bbb97431be26333fb7e7ee50844
pouchine <1291980381@qq.com>
2022-11-19 16:50:04 +0800

    data example

---
+++ student-service/src/main/java/com/example/studentservice/constants/Data.java
@@ -0,0 +1,17 @@
+package com.example.studentservice.constants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author pouchine
+ * @email 120L022131@stu.hit.edu.cn
+ * @create 2022/11/19 16:46
+ */
+public class Data {
+    public static final String top = "Xiao ming";
+    public static final Map<String , Integer> findStudentId = new HashMap<>();
+    static {
+        findStudentId.put("Xiao ming", 24);
+    }
+}
